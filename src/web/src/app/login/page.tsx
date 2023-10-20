6fe9e477cb20f313fd6c2cf33e1294faeb071cb3
Igor Costa <100178056+Igortdcosta@users.noreply.github.com>
2023-10-20 01:20:48 -0300

    chore: adicionado pacote de icones
    Adicionado a parte de login, o pacote de icones e a parte inicial do CSS
    
    Também foi adicionado a logo estilizada

--- src/web/src/app/login/page.tsx
+++ src/web/src/app/login/page.tsx
@@ -1,14 +1,56 @@
 'use client'
 import Link from "next/link";
+
+import Image from "next/image";
 import { useTheme } from "@/shared/hooks/Theme";
+import style from "./login.module.scss";
+import {FaKey} from "react-icons/fa"
 
 export default function Login() {
   const { setTheme } = useTheme();
   setTheme('light');
+
+  
+
   return (
-  <>
-    <h1>Login</h1>
-    <Link href='/'>Go Home</Link>
-  </>
+
+    <div className={style.container}>
+    
+    <header>
+
+        <Image
+          src="/logo.svg"
+          width={120}
+          height={64}
+          alt="TaskIt" />
+
+    </header>
+
+      <main>
+        <h1>Login</h1>
+        <form>
+          <div className={style.inputgroup}>
+          <label htmlFor="email">
+            <input id="email" type="email" placeholder="E-mail" />
+          </label>
+          <label className={style.label} htmlFor="password">
+            <FaKey/>
+            <input id="password" type="password" placeholder="Password" />
+          </label>
+          
+          <button type="submit">Login</button>
+          </div>
+
+        </form>
+        
+      </main>
+
+      <Link href='/'>Go Home</Link>
+    </div>
   )
 }
+
+
+
+
+
