b5a624a9dc8d003121b750e6d31ac8a81dc2ada2
Igor Costa <100178056+Igortdcosta@users.noreply.github.com>
2023-10-23 23:23:05 -0300

    chore: finalizando a tela de login
    Criando a de cadastro

--- src/web/src/app/login/page.tsx
+++ src/web/src/app/login/page.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "@/shared/hooks/Theme";
 import style from "./login.module.scss";
-import {FaKey} from "react-icons/fa"
+import {FaKey} from "react-icons/fa";
+import {AiFillMail} from "react-icons/ai"
 
 export default function Login() {
   const { setTheme } = useTheme();
@@ -30,7 +31,8 @@ export default function Login() {
         <h1>Login</h1>
         <form>
           <div className={style.inputgroup}>
-          <label htmlFor="email">
+          <label className={style.label} htmlFor="email">
+            <AiFillMail/>
             <input id="email" type="email" placeholder="E-mail" />
           </label>
           <label className={style.label} htmlFor="password">
@@ -40,11 +42,34 @@ export default function Login() {
           
           <button type="submit">Login</button>
           </div>
-
+        </form>
+<br />
+        <h1>Cadastro</h1>
+        <form>
+          <div className={style.inputgroup}>
+          <label className={style.label} htmlFor="Nome">
+            <input id="name" type="name" placeholder="Name" />
+          </label>
+          <label className={style.label} htmlFor="LastName">
+            <input id="lastname" type="lastname" placeholder="LastName" />
+          </label>
+          <label className={style.label} htmlFor="email">
+            <input id="email" type="email" placeholder="E-mail" />
+          </label>
+          <label className={style.label} htmlFor="password">
+            <input id="password" type="password" placeholder="Password" />
+          </label>
+          <label className={style.label} htmlFor="checkpassword">
+            <input id="checkpassword" type="checkpassword" placeholder="CheckPassword" />
+          </label>
+          
+          
+          <button type="submit">Cadastrar</button>
+          </div>
         </form>
         
       </main>
-
+<br />
       <Link href='/'>Go Home</Link>
     </div>
   )
